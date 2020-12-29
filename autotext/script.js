12b2c146769bf0fab40bc7c759747fde6525a885
James Anthony Davies <36249882+Jdavies00@users.noreply.github.com>
2020-12-29 16:36:55 -0500

    more text

--- autotext/script.js
+++ autotext/script.js
@@ -1,6 +1,6 @@
 const textEl = document.getElementById('text')
 const speedEl = document.getElementById('speed')
-const text = 'We Love Programming!'
+const text =" Ullamco nulla veniam adipisicing adipisicing. Adipisicing laborum irure nostrud nostrud ullamco nostrud eiusmod commodo eiusmod do duis duis minim voluptate. Ipsum cupidatat pariatur in aliqua ut dolor aute laborum veniam magna esse sint aliqua ea. Cupidatat eiusmod velit mollit officia cupidatat consectetur Lorem deserunt ut qui sunt laboris ut. Nulla esse eu eiusmod consequat incididunt excepteur adipisicing sit ut qui"
 let idx = 1
 let speed = 300 / speedEl.value
 
